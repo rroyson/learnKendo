bd6914c97032939d9dd300be44ef0b5f2cff4e99
Rob Royson <frontiersons@gmail.com>
2019-01-07 14:58:42 -0500

    added grid pagination

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react'
 // You can import style files in ./App.js and add global styles in ./App.css
 import '@progress/kendo-theme-default/dist/all.css'
-import logo from './logo.svg'
 import './App.css'
 
 // Import the Grid component.
@@ -12,6 +11,9 @@ class App extends Component {
     super(props)
 
     this.state = {
+      skip: 0,
+      take: 5,
+      title: 'This is my APP',
       data: [
         { ProductName: 'Chai', Price: 10, Quantity: 7 },
         { ProductName: 'Chang', Price: 20, Quantity: 8 },
@@ -26,16 +28,41 @@ class App extends Component {
         { ProductName: 'Chai', Price: 10, Quantity: 7 },
         { ProductName: 'Chang', Price: 20, Quantity: 8 },
         { ProductName: 'Chai', Price: 10, Quantity: 7 },
+        { ProductName: 'Chang', Price: 20, Quantity: 8 },
+        { ProductName: 'Chai', Price: 10, Quantity: 7 },
         { ProductName: 'Chang', Price: 20, Quantity: 8 }
-      ],
-      title: 'This is my APP'
+      ]
     }
   }
+
+  pageChange = event => {
+    this.setState({
+      skip: event.page.skip,
+      take: event.page.take
+    })
+    console.log(this.state)
+  }
+
   render() {
+    const styles = {
+      gridContainer: { height: '200px' }
+    }
     return (
       <div className="App">
         <h1>{this.state.title} </h1>
-        <Grid data={this.state.data}>
+        <Grid
+          style={styles.gridContainer}
+          data={this.state.data.slice(
+            this.state.skip,
+            this.state.take + this.state.skip
+          )}
+          skip={this.state.skip}
+          take={this.state.take}
+          pageable={true}
+          onPageChange={this.pageChange}
+          pageSize={5}
+          total={this.state.data.length}
+        >
           <GridColumn field="ProductName" title="Product" />
           <GridColumn field="Price" />
           <GridColumn field="Quantity" title="How Many" />
