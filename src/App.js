239c7045bc61c743a9019f889ad19ed2fd22e234
Rob Royson <frontiersons@gmail.com>
2019-01-07 13:58:22 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -2,27 +2,35 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+// Import the Grid component.
+import { Grid, GridColumn } from '@progress/kendo-react-grid';
+
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+ constructor(props) {
+   super(props);
+
+   this.state = {
+     data: [
+       {ProductName: "Chai", Price: 10},
+       {ProductName: "Chang", Price: 20}
+     ],
+     title: "Hello from KendoReact!"
+   }
+ }
+ render() {
+   return (
+     <div className="App">
+       <header className="App-header">
+         <img src={logo} className="App-logo" alt="logo" />
+         <h1 className="App-title">{this.state.title}</h1>
+       </header>
+         <Grid data={this.state.data}>
+           <GridColumn field="ProductName" title="Product name"/>
+           <GridColumn field="Price"/>
+         </Grid>
+     </div>
+   );
+ }
 }
 
 export default App;
